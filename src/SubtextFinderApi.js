f8878e2ea72df4cf1db9fac211da433ce9ea2906
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2022-06-09 22:52:47 +0800

    Fixed API Url

--- src/SubtextFinderApi.js
+++ src/SubtextFinderApi.js
@@ -5,7 +5,7 @@ const API_URL= 'https://subtext-finder-api-resource.azure-api.net/v1';
 const GetIndexesOfMatchedSubtext = (text, subtext) => {
   console.log(`text: ${text}, subtext: ${subtext}`);
   // /?text=Hello%20Wendy%20Sanarwanto.%20How%20are%20you%20today,%20Wendy%20?&subtext=Wendy
-  let apiParams = `/>text=${text}?&subtext=${subtext}`;
+  let apiParams = `/?text=${text}&subtext=${subtext}`;
   let apiUrl = encodeURI(`${API_URL}${apiParams}`);
   console.log(`apiUrl=${apiUrl}`);
   return axios.get(apiUrl);
