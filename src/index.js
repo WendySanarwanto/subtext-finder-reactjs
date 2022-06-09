9387e39800861fdff97a399d0dd7e4c559c30076
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2022-06-09 21:08:11 +0800

    Added mocked input form

--- src/index.js
+++ src/index.js
@@ -8,6 +8,32 @@ const App = () => {
   return (
     <div className="ui container">
       <h1>Find subtext on a Text </h1>
+      <hr />
+      
+      <div className="ui segment">
+        <div className="ui form">
+            <div className="two fields">            
+              <div className="field">
+                <label>Enter text</label>
+                <input type="text" placeholder="Text"/> 
+              </div>
+              <div className="field">
+                <label>Enter Subtext</label>
+                <input type="text" placeholder="Subtext to search"/> 
+              </div>
+            </div>
+            <div class="ui submit primary button">Process</div>
+        </div>
+      </div>
+
+      <div className="ui segment">
+        <div className="ui vertical segment">          
+          <h2>Hello Wendy Sanarwanto. How are you today, Wendy ?</h2>
+        </div>
+        <div className="ui vertical segment">
+          <p>Found matched subtext on index 6 and 43.</p>
+        </div>
+      </div>
     </div>
   );
 };
